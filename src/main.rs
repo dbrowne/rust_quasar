5aa34ad1ddd9502bcbae38d578a3b79f3109817a
dbrowne <dwight@dwightjbrowne.com>
2021-11-10 00:17:43 -0700

    added process spawning

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,17 @@
 //! Dwight J. Browne
 //!
 
-use std::{thread, time};
 use std::env;
 use git_version::git_version;
 use structopt::StructOpt;
-use std::time::{Duration, Instant};
+use std::time::Instant;
+use nix::sys::wait::wait;
+use nix::unistd::ForkResult::{Child, Parent};
+use nix::unistd::{fork, getpid, getppid};
+
+
+use std::process::{Command, exit};
+
 
 // Exponential complexity to consume resources
 
@@ -18,15 +24,19 @@ fn fib(inp: u64) -> u64 {
     }
 }
 
-fn fib_wrapper(inp: u64) {
+fn fib_wrapper(inp: &i32) {
     let start = Instant::now();
 
-    let ret_val = fib(inp);
+    let xx = *inp as u64;
+    let ret_val = fib(xx);
     let duration = start.elapsed();
     println!("Done with fib of {} = {} for {:?} seconds",inp, ret_val, duration);
 }
 
+
 fn main() {
+    const UPPER: i32 = 55;
+    const LOWER: i32 = 1;
     // check for command line args
     #[derive(StructOpt, Debug)]
     // #[derive(Debug)]
@@ -38,6 +48,8 @@ fn main() {
 
     let opt = Opt::from_args();
     let args: Vec<String> = env::args().collect();
+    println!("args = {:?}",args);
+
     let _func: i32;
     if args.len() > 1 {
         _func = opt.func;
@@ -50,8 +62,32 @@ fn main() {
         Ok(host_name) => host_name
     };
     println!("Quasar version ->{}<- on host {:?}\n", GIT_VERSION, host_name);
+    let pid = getpid();
+    println!("this is pid {}",pid);
     match _func {
-        0 => fib_wrapper(46),
-        _ => println!("Got something other than default {}", _func),
+        0 => fib_wrapper(&_func),
+        LOWER..=UPPER=>fib_wrapper(&_func),
+        _ => {
+            println!("Got something other than default {}", _func);
+            exit(-1);
+        },
+    }
+
+    let pid = unsafe {fork()};
+
+
+    match pid.expect("Failed Fork: Cannot create child process!!"){
+        Child => {
+            println!("This is the child process: pid = {}, parent pid = {}", getpid(), getppid());
+            println!("Calling command {} ",args[0].to_string());
+            let tt = _func+1;
+            let xxx = Command::new(&args[0].to_string()).args(["-f",&tt.to_string()]).status().expect("failed to execute process");
+            println!("Called {:?}",xxx);
+        },
+        Parent { child} =>{
+
+            let _res = wait();
+            println!("This is the parent pid = {}, child= {}",getpid(), child);
+        }
     }
 }
